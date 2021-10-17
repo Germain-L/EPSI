95e46cbffb629fd9695756679777e0316bb3441e
Germain <germain.leignel@gmail.com>
2021-10-17 15:54:54 +0200

    c#

--- B2/C#/RPS/Program.cs
+++ B2/C#/RPS/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using RPS.Players;
 
+
 namespace RPS
 {
     class Program
@@ -8,14 +9,22 @@ namespace RPS
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to rock paper scissors !");
-
-            while (true)
+            bool playAgain = true;
+            while (playAgain)
             {
                 Game game = new Game();
                 Player winner  = game.Play();
                 
                 Console.WriteLine($"{winner} wins\n");
+                Console.WriteLine("Do you wish to play again ? (Y/n)");
+                var choice = Console.ReadLine().ToLower();
+                if (choice == "N")
+                {
+                    playAgain = false;
+                }
             }
+            
+            Console.WriteLine("Goodbye :D");
         }
     }
 }
