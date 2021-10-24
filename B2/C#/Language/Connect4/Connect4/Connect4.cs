784f681273275baf5d4fc54130997d2caa852de5
Germain-L <germain.leignel@gmail.com>
2021-10-24 22:06:07 +0200

    please fix diag checking

--- B2/C#/Language/Connect4/Connect4/Connect4.cs
+++ B2/C#/Language/Connect4/Connect4/Connect4.cs
@@ -10,46 +10,45 @@ namespace Puissance4
 
         private bool _alternator = false;
         private char[,] _board = new char[7, 6];
-        private int _winner;
+        private int _winner = 0; // If never changed then draw
+        private bool _ended = false;
 
         #endregion
 
-        // TODO : Compléter cette classe pour implémenter les règles du puissance 4
-        // Etape 1
         public int LineCount => 6;
 
         public int ColCount => 7;
 
-        // Etape 2
         // TODO : Utiliser un tableau à deux dimensions
         public char GetPawn(int col, int line) => _board[col, line];
 
-        // Etape 3
-        // Remplir la colonne jouée et changer de joueur
         public int PlayerNumber
         {
             get => _alternator ? 1 : 2;
         }
 
-        public bool Play(int column)
+        public bool Play(int col)
         {
-            column--;
+            col--;
             bool placed = false;
 
             for (int line = LineCount - 1; line >= 0; line--)
             {
-                if (_board[column, line] != 'X' && _board[column, line] != 'O')
+                if (_board[col, line] != 'X' && _board[col, line] != 'O')
                 {
                     // Placing pawn
-                    _board[column, line] = _alternator ? 'X' : 'O';
+                    _board[col, line] = _alternator ? 'X' : 'O';
                     placed = true;
 
                     // get next player
                     _alternator = !_alternator;
 
-                    Console.WriteLine(_checkVer(line: line, col: column));
-                    Console.WriteLine(_checkHor(line: line, col: column));
-                    Console.WriteLine(_checkDiags(line: line, col: column));
+                    if (_checkVer(line, col) || _checkHor(line, col) || _checkDiags(line, col))
+                    {
+                        _winner = PlayerNumber;
+                        Ended = true;
+                    }
+
                     break;
                 }
             }
@@ -57,15 +56,19 @@ namespace Puissance4
             return placed;
         }
 
-        // Etape 4 
-        // Améliorer le Play pour qu'il détecte la victoire/nul et implémenter Winner et Ended
         public int Winner
         {
-            set => value = _winner;
+            private set => value = _winner;
             get => _winner;
         }
 
-        public bool Ended => false;
+        public bool Ended
+        {
+            get => _ended;
+            private set => _ended = value;
+        }
+
+        #region checks
 
         private bool _checkVer(int line, int col)
         {
@@ -129,45 +132,45 @@ namespace Puissance4
 
         private bool _checkDiags(int line, int col)
         {
-            char piecePlayed = _board[col, line];
-
-            int lineIndex;
-            int colIndex;
-
-            int consecutive = 0;
-
-            // check topleft - bottom right
-            lineIndex = line + 4;
-            colIndex = line - 4;
-            while (lineIndex < line - 4 && colIndex < col + 4)
-            {
-                // check for index errors
-                if (lineIndex > LineCount - 1
-                    || lineIndex < 0
-                    || colIndex > ColCount
-                    || colIndex < 0)
-                {
-                    continue;
-                }
-            
-                if (_board[colIndex, lineIndex] == piecePlayed)
-                {
-                    consecutive++;
-                }
-                else
-                {
-                    consecutive = 0;
-                }
-            
-                if (consecutive >= 4)
-                {
-                    return true;
-                }
-                // line goes down (decrease)
-                // column goes right (increase)
-                lineIndex--;
-                colIndex++;
-            }
+            // char piecePlayed = _board[col, line];
+            //
+            // int lineIndex;
+            // int colIndex;
+            //
+            // int consecutive = 0;
+            //
+            // // check topleft - bottom right
+            // lineIndex = line + 4;
+            // colIndex = line - 4;
+            // while (lineIndex < line - 4 && colIndex < col + 4)
+            // {
+            //     // check for index errors
+            //     if (lineIndex > LineCount - 1
+            //         || lineIndex < 0
+            //         || colIndex > ColCount
+            //         || colIndex < 0)
+            //     {
+            //         continue;
+            //     }
+            //
+            //     if (_board[colIndex, lineIndex] == piecePlayed)
+            //     {
+            //         consecutive++;
+            //     }
+            //     else
+            //     {
+            //         consecutive = 0;
+            //     }
+            //
+            //     if (consecutive >= 4)
+            //     {
+            //         return true;
+            //     }
+            //     // line goes down (decrease)
+            //     // column goes right (increase)
+            //     lineIndex--;
+            //     colIndex++;
+            // }
 
             // lineIndex = line + 4;
             // colIndex = col - 4;
@@ -202,5 +205,8 @@ namespace Puissance4
 
             return false;
         }
+        
+        #endregion
+
     }
 }
