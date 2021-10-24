96b56531f0dcfd60b5fae88c0084d1806374068b
Germain-L <germain.leignel@gmail.com>
2021-10-24 12:17:54 +0200

    connect4 is playable

--- B2/C#/Language/Connect4/Connect4/Connect4.cs
+++ B2/C#/Language/Connect4/Connect4/Connect4.cs
@@ -6,25 +6,59 @@ namespace Puissance4
 {
     class Connect4
     {
+        #region privates
+
+        private bool _alternator = false;
+        private char[,] _board = new char[7, 6];
+
+        #endregion
+
         // TODO : Compléter cette classe pour implémenter les règles du puissance 4
         // Etape 1
-        public int LineCount
-        {
-            get { return 2; }
-        }
-        public int ColCount => 3;
+        public int LineCount => 6;
+        public int ColCount => 7;
 
         // Etape 2
         // TODO : Utiliser un tableau à deux dimensions
-        public char GetPawn(int col, int line) => ' ';
+        public char GetPawn(int line, int col) => _board[line, col];
 
         // Etape 3
         // Remplir la colonne jouée et changer de joueur
-        public int PlayerNumber => 1;
-
-        public void Play(int column)
+        public int PlayerNumber
         {
+            get
+            {
+                // get next player
+                _alternator = !_alternator;
+                return _alternator ? 1 : 2;
+            }
+        }
 
+        public bool Play(int column)
+        {
+            column--;
+            bool placed = false;
+            // for (int i = LineCount - 1; i > 0; i--)
+            // {
+            //     if (GetPawn(i, column) != 'X' && GetPawn(i, column) != 'X')
+            //     {
+            //         _board[column, i] = _alternator ? 'O' : 'X';
+            //         placed = true;
+            //         break;
+            //     }
+            // }
+
+            for (int line = LineCount - 1; line > 1; line--)
+            {
+                if (_board[column, line] != 'X' && _board[column, line] != 'O')
+                {
+                    Console.WriteLine($"Playing {column}, {line}");
+                    _board[column, line] = _alternator ? 'X' : 'O';
+                    break;
+                }
+            }
+
+            return placed;
         }
 
         // Etape 4 
--- B2/C#/Language/Connect4/Connect4/Program.cs
+++ B2/C#/Language/Connect4/Connect4/Program.cs
@@ -11,7 +11,7 @@ namespace Puissance4
             do
             {
                 Display(game);
-                for (; ; )
+                for (;;)
                 {
                     Console.WriteLine($"Player {game.PlayerNumber} : Which column 1-{game.ColCount} ?");
 
@@ -23,12 +23,13 @@ namespace Puissance4
                         game.Play(column);
                         break;
                     }
+
                     Console.Error.WriteLine("Invalid column number.");
                 }
-            }
-            while (!game.Ended);
+            } while (!game.Ended);
+
             Display(game);
-            if(game.Winner == 0)
+            if (game.Winner == 0)
             {
                 Console.WriteLine("Draw");
             }
@@ -37,6 +38,7 @@ namespace Puissance4
                 Console.WriteLine($"Player {game.Winner} wins.");
             }
         }
+
         private static void Display(Connect4 game)
         {
             for (int y = 0; y < game.LineCount; y++)
@@ -45,17 +47,21 @@ namespace Puissance4
                 {
                     Console.Write($"| {game.GetPawn(x, y)} ");
                 }
+
                 Console.WriteLine("|");
                 for (int x = 0; x < game.ColCount; x++)
                 {
                     Console.Write($"+---");
                 }
+
                 Console.WriteLine("+");
             }
+
             for (int x = 0; x < game.ColCount; x++)
             {
                 Console.Write($"  {x + 1} ");
             }
+
             Console.WriteLine();
         }
     }
