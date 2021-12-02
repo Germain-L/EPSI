6f11ed2425145588732e891979325944361f3806
Germain Leignel <germain.leignel@gmail.com>
2021-12-02 13:57:26 +0100

    flask progress

--- B2/python/chess_flask/app.py
+++ B2/python/chess_flask/app.py
@@ -1,18 +1,38 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from models.chessboard import ChessBoard
+from pprint import pprint
 
 app = Flask(__name__)
-chess = ChessBoard()
+chessBoard = ChessBoard()
 
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    return render_template("chess.html")
 
 
 @app.route('/chess')
 def chess():  # put application's code here
-    return render_template("chess.html")
+    board = []
+
+    to = request.args.get("move")
+    original = request.args.get("original")
+    undo = request.args.get("to")
+
+    for y in range(0, 8):
+        row = []
+        for x in range(0, 8):
+            piece = chessBoard.get_piece(x, y)
+            symbol = ""
+            if piece is not None:
+                symbol = piece.get_piece().get_symbol(0)
+            row.append(symbol)
+        board.append(row)
+
+    for i in board:
+        print(i)
+
+    return render_template("dynamic.html", board=board, board_len=len(board), len_row=len(board[0]))
 
 
 if __name__ == '__main__':
